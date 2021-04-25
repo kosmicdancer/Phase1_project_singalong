889bc5e2c446233710c1b33172f7a3822952d6a1
Kyoko <kyokoconnect@gmail.com>
2021-04-25 19:39:15 -0400

    reorganize html, change fetch function to asynch, push preventdefault back

--- index.js
+++ index.js
@@ -3,22 +3,20 @@
 // Declare Functions needed
 //     -fetch data from https://api.lyrics.ovh
 
-
 const baseUrl = 'https://api.lyrics.ovh'
 
-const getSuggest = () => {
-    const searchInput = `${baseUrl}/suggest/:term/?_limit=5`
-    return fetch (searchInput) 
-    .then(r => r.json())
-    
+const getSuggest = async () => {
+    const searchInput = `${baseUrl}/suggest/${term}`
+    const r = await fetch(searchInput);
+    return await r.json();   
 }
 getSuggest().then(objectData => console.log(objectData));
 
-//     -create song-artist suggestion div
+
+//      -create song-artist suggestion div
 const suggestDiv = document.querySelector('#suggest-container')
 
 const createSuggestDiv = (data) => {
-    const div = document.createElement('div'),
         suggestUl = document.createElement('ul'),
         artist_titleli = document.createElement('li');
         
@@ -29,23 +27,24 @@ const createSuggestDiv = (data) => {
     div.appendChild(suggestUl)
     suggestUl.appendChild(artist_titleli)
     artist_titleli.appendChild(title, artist)
-    
-
 
     return div; 
 }
 
+//      -create search box (form)
+const searchForm = document.querySelector('#search-container')
 
+searchForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    const searchInput = evt.target.search.value;
+})
     
 
 
-const testdata = {
-}
+// const testdata = {
+// }
 
 //     -display lyrics
-const createLyricsDiv = () => {
+// const createLyricsDiv = () => {
 
-}
-
-
-
+// }
